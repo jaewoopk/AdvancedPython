4014c0226aee79b5ec4f698ea7ebb6d78b9db03e
jaewoopk <jaewoopk000@naver.com>
2023-03-16 15:27:28 +0900

    Day 2 -

--- Day2.py
+++ Day2.py
@@ -1,7 +1,19 @@
+# dice1 = int(input())
+# dice2 = int(input())
+
+# if (dice1 % 2 == 0 and dice2 % 2 == 0) :
+#     print("All dices are even!")
+# elif (dice1 % 2 == 1 and dice2 % 2 == 1) :
+#     print("All dices are odd!")
+
 dice1 = int(input())
 dice2 = int(input())
 
-if (dice1 % 2 == 0 and dice2 % 2 == 0) :
-    print("All dices are even!")
-elif (dice1 % 2 == 1 and dice2 % 2 == 1) :
-    print("All dices are odd!")
+sum = dice1 + dice2
+
+if (sum % 3 == 0 and sum % 6 == 0) :
+    print("%d is multiple of 3 and 6"%sum)
+elif (sum % 3 == 0) :
+    print("%d is multiple of 3"%sum)
+else :
+    print("%d is not multiple of 3 or 6"%sum)
