3407573a6bf1aae23a79f61ad490cc2e913aa132
jaewoopk <jaewoopk000@naver.com>
2023-03-16 13:43:00 +0900

    Day 2 -

---
+++ Day2.py
@@ -0,0 +1,7 @@
+dice1 = int(input())
+dice2 = int(input())
+
+if (dice1 % 2 == 0 and dice2 % 2 == 0) :
+    print("All dices are even!")
+elif (dice1 % 2 == 1 and dice2 % 2 == 1) :
+    print("All dices are odd!")
