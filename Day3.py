df8c5c08016dc9fb0496fedb7d97c6787bf3f332
jaewoopk <jaewoopk000@naver.com>
2023-03-23 13:27:39 +0900

    Day 3 -

---
+++ Day3.py
@@ -0,0 +1,27 @@
+# name = input()
+# residence = input()
+# year = int(input())
+
+# print("Name : %s"%name)
+# print("Residence : %s"%residence)
+# print("Birth Year : %d"%(2023 - year + 1))
+
+
+# m = int(input())
+
+# km = m / 1000
+# mile = m / 1609
+# inch = m * 39.37
+# feet = m * 3.281
+
+# print("%d m to km = %.2f km"%(m, km))
+# print("%d m to mile = %.2f mile"%(m, mile))
+# print("%d m to inch = %.2f inch"%(m, inch))
+# print("%d m to feet = %.2f feet"%(m, feet))
+
+n = int(input())
+m = int(input())
+
+realM = (m // 10) + (m % 10)
+
+print("Result = %d"%(n * realM))
